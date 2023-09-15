3825cf2c1270b6b8777ade02795799bb44acf509
Prof-Oj01 <ojoofe20@gmail.com>
2023-09-15 03:35:20 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-#include <unstid.h>
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -6,7 +6,7 @@
  * Return: On success 1. On error, -1 is returned, and rrror is set apprppriately.
  */
 
-int_putchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
