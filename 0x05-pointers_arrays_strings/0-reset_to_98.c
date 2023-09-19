5789d7cc188306c34742d42a5fda88106873e45c
Prof-Oj01 <ojoofe20@gmail.com>
2023-09-18 21:48:49 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - fnction that takes a pointer to an int
+ * @n: funtion parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is the readme for the pointer, arrays and strings tasks
