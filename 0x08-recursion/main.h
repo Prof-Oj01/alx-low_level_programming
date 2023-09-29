95cabfe356d29c2a443669346cc8a4e6296ec339
Prof-Oj01 <ojoofe20@gmail.com>
2023-09-28 22:13:31 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,6 +12,10 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int sqrt_a(int a, int b);
+int prime_a (int a, int b);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int compare_string(char *s, int left, int right);
 
 #endif /* MAIN_H */
 
