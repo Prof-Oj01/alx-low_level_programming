cf0882631959007bdc3b62da672f4f345b832295
Prof-Oj01 <ojoofe20@gmail.com>
2023-10-11 21:37:27 -0700

    calc func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * get_op_func - function that selects the option to perform arithmetic operation
@@ -11,12 +12,12 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+		{"+", op_add},
+        	{"-", op_sub},
+        	{"*", op_mul},
+        	{"/", op_div},
+        	{"%", op_mod},
+        	{NULL, NULL}
     };
     int i;
 
@@ -24,7 +25,7 @@ int (*get_op_func(char *s))(int, int)
     i = 0;
 
     /* s does not match any of the operands */
-    while (ops[i].op != NULL && (ops[i].op) != *s)
+    while (ops[i].op != NULL && (ops[i].op) != s)
 	    i++;
 
     return (ops[i].f); /* return operation */
