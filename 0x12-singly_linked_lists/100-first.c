8c27dd639e47f66913e6b560c5775c27da5033fb
Prof-Oj01 <ojoofe20@gmail.com>
2023-10-19 18:39:28 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+void Come_firstbyOJ(void)__attribute__ ((constructor));
+
+/**
+ * Come_first - func that runs before the main func
+ */
+
+void Come_firstbyOJ(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
