e92fb1bad5e54324b8f4470277dbdfecccd91dc2
Prof-Oj01 <ojoofe20@gmail.com>
2023-10-20 18:59:02 -0700

    3-add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,74 +1,37 @@
 #include "lists.h"
-int len(const char *str);
-list_t *create_node(const char *str);
-
 /**
- * add_node_end - func that adds node to the end of a list
- * @head: pointer to the first node
- * @str: new node to be added at the end
- * Return: pointer to the head
- */
+ * add_node_end - Add a new node at the end of a list
+ * @head: Address of the first node of a list
+ * @str: Address of the string to insert into the new node
+ * Return: Address of the new node
+ **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *tmp;
-	list_t *new_node;
-
-	tmp = *head;
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-		new_node = create_node(str);
-	if (new_node == NULL)
-		return (NULL);
-	if (*head == NULL)
-
-	{
-		*head = new_node;
-		return (*head);
-	}
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = new_node;
-	return (*head);
-}
-
-
-/**
- * create_node: func that declares nodes
- * @str: string to be inputrd in the newly created node
- * Return: the pointer to the malloc memory
- */
-list_t *create_node(const char *str)
+list_t *tmp, *tmp2;
+unsigned int length = 0;
+if (str == NULL)
+return (NULL);
+tmp = malloc(sizeof(list_t));
+if (tmp == NULL)
+return (NULL);
+tmp->str = strdup(str);
+if (tmp->str == NULL)
 {
-	list_t *new_node;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->str = strdup(str);
-	new_node->len = len(str);
-	new_node->next = NULL;
-	return (new_node);
+free(tmp);
+return (NULL);
 }
-
-
-/**
- * len - length of a string
- * @str: sttring whose length is to be found
- * Return: amount of length
- */
-int len(const char *str)
+while (str[length])
+length++;
+tmp->len = length;
+tmp->next = NULL;
+if (*head == NULL)
 {
-	int i;
-
-	if (str == NULL)
-		return (0);
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+*head = tmp;
+return (tmp);
+}
+tmp2 = *head;
+while (tmp2->next)
+tmp2 = tmp2->next;
+tmp2->next = tmp;
+return (tmp);
 }
